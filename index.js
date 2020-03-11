23ddf18dddaa7a3ee3431dc27ee6c06ea381457e
Dorian Hemerick <dorianhemerick@gmail.com>
2020-03-11 02:16:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-var kittens = ["Milo", "Otis", "Garfield"] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"]
 
-// Add your functions and code here
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+}
